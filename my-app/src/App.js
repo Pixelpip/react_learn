2942e7ae9b42f8d9175c06fc0166bcf1832bbf89
Pixelpip <athulkamathg5@gmail.com>
2023-12-06 00:10:28 +0530

    1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+const user={
+  name:"Atul",
+  branch:"cs"
+};
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function Myapp(){
+  return(
+  <button>Helo</button>
   );
 }
 
-export default App;
+const Abc=({user})=>{
+  return(
+  <>
+  <h1>{user.name}</h1>
+  <h2>{user.branch}</h2>
+
+  <Myapp/>
+  </>
+  );
+};
+
+export default Abc;
+
+
